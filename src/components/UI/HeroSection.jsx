e41e44417aeba8b1d9f6559c14cc3f288d400e14
Anshuman <anshumanmr2001@gmail.com>
2025-04-16 02:41:03 +0530

    change image url

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           </button>
         </div>
         <div className='hero-image'>
-          <img className="world-map" src="/public/images/world.png" alt="" />
+          <img className="world-map" src="https://atlas-lp3xc1beo-anshumanmajumders-projects.vercel.app/images/world.png" alt="" />
         </div>
         </div>
        </main>
